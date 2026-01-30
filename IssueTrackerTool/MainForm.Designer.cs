0194be1e13da6a30b7580b0b030f336214f1a30f
Rinse Cramer <rinsecramer@gmail.com>
2026-01-30 10:55:32 +0100

    Add timer controls and event handlers to MainForm
    Added StatusLabel, StartTimerButton, StopTimerButton, and a Timer to MainForm. Configured Timer for 60-second intervals. Implemented event handlers to start, stop, and auto-disable the Timer. Updated designer and resource files to include new controls and Timer metadata.

--- IssueTrackerTool/MainForm.Designer.cs
+++ IssueTrackerTool/MainForm.Designer.cs
@@ -28,22 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.StartTimerButton = new System.Windows.Forms.Button();
+            this.StopTimerButton = new System.Windows.Forms.Button();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Location = new System.Drawing.Point(13, 13);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(114, 13);
+            this.StatusLabel.TabIndex = 0;
+            this.StatusLabel.Text = "Status = special action";
+            // 
+            // StartTimerButton
+            // 
+            this.StartTimerButton.Location = new System.Drawing.Point(13, 30);
+            this.StartTimerButton.Name = "StartTimerButton";
+            this.StartTimerButton.Size = new System.Drawing.Size(100, 100);
+            this.StartTimerButton.TabIndex = 1;
+            this.StartTimerButton.Text = "Start timer (ETC)";
+            this.StartTimerButton.UseVisualStyleBackColor = true;
+            // 
+            // StopTimerButton
+            // 
+            this.StopTimerButton.Location = new System.Drawing.Point(120, 30);
+            this.StopTimerButton.Name = "StopTimerButton";
+            this.StopTimerButton.Size = new System.Drawing.Size(100, 100);
+            this.StopTimerButton.TabIndex = 2;
+            this.StopTimerButton.Text = "Stop timer (ETC)";
+            this.StopTimerButton.UseVisualStyleBackColor = true;
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 60000;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StopTimerButton);
+            this.Controls.Add(this.StartTimerButton);
+            this.Controls.Add(this.StatusLabel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "Issue Tracker Tool";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Button StartTimerButton;
+        private System.Windows.Forms.Button StopTimerButton;
+        private System.Windows.Forms.Timer Timer;
     }
 }
 
--- IssueTrackerTool/MainForm.cs
+++ IssueTrackerTool/MainForm.cs
@@ -17,11 +17,32 @@ namespace IssueTrackerTool
             InitializeComponent();
             // referentie: https://github.com/microsoft/winappCli/blob/main/src/winapp-GUI/winapp-GUI/MainWindow.xaml.cs#L62
             SetupWindow();
+            // referentie: https://github.com/microsoft/winappCli/blob/main/src/winapp-GUI/winapp-GUI/MainWindow.xaml.cs#L68
+            StopTimerButton.Click += StopTimerButton_Click;
+            // referentie: https://github.com/microsoft/winappCli/blob/main/src/winapp-GUI/winapp-GUI/MainWindow.xaml.cs#L72
+            StartTimerButton.Click += StartTimerButton_Click;
+            // referentie: https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.timer?view=netframework-4.7.2#examples
+            Timer.Tick += Timer_Tick;
         }
         // referentie: https://github.com/microsoft/winappCli/blob/main/src/winapp-GUI/winapp-GUI/MainWindow.xaml.cs#L81
         private void SetupWindow()
         {
             Size = new Size(1000, 1000);
+        }
+        // referentie: https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.timer?view=netframework-4.7.2#examples
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Timer.Enabled = false;
+        }
+        // referentie: https://github.com/microsoft/winappCli/blob/main/src/winapp-GUI/winapp-GUI/MainWindow.xaml.cs#L136
+        private void StartTimerButton_Click(object sender, EventArgs e)
+        {
+            Timer.Enabled = true;
+        }
+        // referentie: https://github.com/microsoft/winappCli/blob/main/src/winapp-GUI/winapp-GUI/MainWindow.xaml.cs#L523
+        private void StopTimerButton_Click(object sender, EventArgs e)
+        {
+            Timer.Enabled = false;
         }
     }
 }
